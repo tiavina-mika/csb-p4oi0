0aa7de755a9b406c063bab46ea1c51a08942bfe3
Code Sandbox <hello@codesandbox.io>
2021-11-29 08:01:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.00d7cecaeedd02b5afcab1833e5baddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c47262cd2b257334d799",
+    "url": "https://tiavina-mika.github.io/csb-p4oi0/static/css/main.7d7da4e8.chunk.css"
+  },
+  {
+    "revision": "c47262cd2b257334d799",
+    "url": "https://tiavina-mika.github.io/csb-p4oi0/static/js/main.c47262cd.chunk.js"
+  },
+  {
+    "revision": "2872cfef2b1c052dfcbe",
+    "url": "https://tiavina-mika.github.io/csb-p4oi0/static/js/1.2872cfef.chunk.js"
+  },
+  {
+    "revision": "6860958e42ba8f765eb6",
+    "url": "https://tiavina-mika.github.io/csb-p4oi0/static/js/runtime~main.6860958e.js"
+  },
+  {
+    "revision": "8ad36025a2b4a5eb0a9f1b66b6df7f16",
+    "url": "https://tiavina-mika.github.io/csb-p4oi0/index.html"
+  }
+];
